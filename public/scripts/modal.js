21420e26552814d597cf644607c5fcd676e26033
SirMechT <69607230+SirMechT@users.noreply.github.com>
2021-07-06 11:05:42 -0300

    refazendo main/modal.js

---
+++ public/scripts/modal.js
@@ -0,0 +1,16 @@
+// a Function Modal foi exportada como módulo 
+export default function Modal(){
+    function open(){
+        // funcionalidade de atribuir a classe active para a modal
+        document.querySelector('.modal-wrapper').classList.add("active")
+    }
+    function close(){
+        // funcionalidade de remover a classe active da modal
+
+    }
+
+    return{
+        open,
+        close
+    }
+}
--- src/route.js
+++ src/route.js
@@ -4,6 +4,8 @@ const express = require('express')
 const route = express.Router()
 
 route.get('/', (req, res) => res.render("index.ejs"))
+route.get('/room', (req, res) => res.render("room.ejs"))
+route.get('/create-pass', (req, res) => res.render("create-pass.ejs"))
 
 // para o route ser usado é necessárop exportá-lo do arquivos route.js e importar quando for necessário em algum outro arquivo
 module.exports = route
