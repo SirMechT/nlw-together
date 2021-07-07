9fa7538a7b1149307c7f71504f84cfc769720cae
SirMechT <69607230+SirMechT@users.noreply.github.com>
2021-07-07 18:38:56 -0300

    aula04(finalizado)

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,7 +1,31 @@
-module.exports = {
-    create(req, res){
-        let roomId = 323243
+// import do Banco de dados (Db)
+const Database = require("../db/config")
 
+module.exports = {
+    async create(req, res){
+        // inicialização do DB no controllers
+        const db = await Database()
+        // inclusão do password vindo do create-pass.ejs
+        var pass = req.body.password
+        let roomId
+
+        for(var i =0; i < 6; i++){
+            i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
+            roomId += Math.floor(Math.random() * 10).toString()
+        }
+
+        console.log(pass)
+
+        // inserção de dados na tabela 
+        await db.run(`INSERT INTO rooms (
+            id,
+            pass
+        ) VALUES (
+            ${parseInt(roomId)}, 
+            ${pass}
+        )`)
+        
+        await db.close()
 
         // comando de redirecionamento da página com o ID da room
         res.redirect(`/room/${roomId}`)
--- src/db/config.js
+++ src/db/config.js
@@ -6,6 +6,6 @@ const { open } = require("sqlite")
 module.exports = () =>
     open({
         // caminho para o banco de dados
-        filename: '/src/db/rocketq.sqlite',
+        filename: './src/db/rocketq.sqlite',
         driver: sqlite3.Database,
     })
